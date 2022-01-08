803eefb1821887c67fd1a24751cc1f191be76ab1
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-01-08 00:35:36 -0800

    added comments and another dummy route

--- frontend/src/pages/public/components/Footer.js
+++ frontend/src/pages/public/components/Footer.js
@@ -13,6 +13,7 @@ function Footer() {
         >
             <div
                 onClick={() => {
+                    // http://127.0.0.1:8080/api/foo
                     let testingURL = process.env.REACT_APP_API_URL + "/foo";
                     console.log("testingUrl:", testingURL);
                     fetch(testingURL, getOptions)
@@ -32,6 +33,7 @@ function Footer() {
             </div>
             <div
                 onClick={() => {
+                    // http://127.0.0.1:8080/api/auth/
                     let testingURL = process.env.REACT_APP_API_URL + "/auth/";
                     console.log("helpCenter, testingURL:", testingURL);
                     fetch(testingURL, getOptions)
@@ -57,8 +59,47 @@ function Footer() {
             >
                 Terms
             </div>
-            <div className="darkmode-text">Privacy Policy</div>
-            <div className="darkmode-text mobile-center">Cookies</div>
+            <div
+                className="darkmode-text"
+                onClick={() => {
+                    // http://127.0.0.1:8080/api/auth/foo
+                    let testingURL =
+                        process.env.REACT_APP_API_URL + "/auth/foo";
+                    console.log("helpCenter, testingURL:", testingURL);
+                    fetch(testingURL, getOptions)
+                        .then((res) => {
+                            res.text().then((resp) => {
+                                console.log(resp);
+                            });
+                            console.log("line 69");
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+                }}
+            >
+                Privacy Policy
+            </div>
+            <div
+                className="darkmode-text mobile-center"
+                onClick={() => {
+                    // http://127.0.0.1:8080/api/
+                    let testingURL = process.env.REACT_APP_API_URL + "/";
+                    console.log("helpCenter, testingURL:", testingURL);
+                    fetch(testingURL, getOptions)
+                        .then((res) => {
+                            res.text().then((resp) => {
+                                console.log(resp);
+                            });
+                            console.log("line 94");
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+                }}
+            >
+                Cookies
+            </div>
             <div className="darkmode-text mobile-right">Ads Info</div>
             <div className="darkmode-text">Blog</div>
             <div className="darkmode-text mobile-center">Status</div>
